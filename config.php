02d641eb7c4f5ff731937a4912afb87d31858fac
Vamour Valentin <vamour@bad.fil.univ-lille1.fr>
2016-04-22 10:02:46 +0200

    Commencement du webservice de connexion

---
+++ config.php
@@ -0,0 +1,3 @@
+<?php
+    $config = array("sqltype"=>"pgsql", "host"=>"localhost", "dbname"=>"vamour", "username"=>"vamour", "password"=>"eU2MCKraESFCTAjp"); 
+?>
--- webconnect.php
+++ webconnect.php
@@ -1,5 +1,38 @@
 <?php
+    require("config.php");
     
+    header('Content-Type: application/json');
+    $answer = array();
     
+    if(isset($_GET['username']) && isset($_GET['password'])) {
+        $username = htmlspecialchars($_GET['username']);
+        $password = $_GET['password'];
+    
+        try{
+            $bdd = new PDO($config["sqltype"].":host=".$config["host"].";dbname=".$config["dbname"], $config["username"], $config["password"]);
+        }
+        catch (PDOException $e) {
+            $answer["status"] = "error";
+            $answer["message"] = $e->getMessage();
+        }
+        
+        $req = $bdd->prepare("SELECT * FROM users WHERE pseudo=:pseudo");
+        $req->exec(array("pseudo"=>$username));
+        $req->fetch();
+        
+        if($req == NULL) {
+            $answer["status"] = "error";
+            $answer["message"] = "Wrong username or password";
+        }
+        
+        
+    }
+    else
+    {
+        $answer["status"] = "error";
+        $answer["message"] = "Unable to find require datas";
+    }
+    
+    echo json_encode($answer);
     
 ?>
