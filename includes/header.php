ee4abf9ea5bae397568fd5b26c259233d226f9a5
Firhos <valentin.vamour@gmail.com>
2016-04-27 20:18:21 +0200

    Commencement de la mise en page de l'index

--- includes/header.php
+++ includes/header.php
@@ -6,10 +6,13 @@
         echo "<div class=\"right\">";
         
         if(!isset($_SESSION['ident'])){
+            echo "<form id='connection_form'>";
             echo "<span id='message'></span>";
                 echo "<span class=\"usericon\"> </span><input id=\"username\" type=\"text\"' />";
                 echo "<span class=\"passwordincon\"> </span><input id=\"password\" type=\"password\" />";
-                echo "<button id='connect' type=\"button\">Connexion</button> ";            
+                echo "<button id='connect' type=\"button\">Connexion</button> ";       
+                echo "</form><br/><a href=\"inscription.html\">S'inscrire</a>";
+                     
         }
         else {
             $user = json_decode($_SESSION['ident'], true);
--- index.php
+++ index.php
@@ -4,12 +4,28 @@
     <head>
         <meta charset="utf-8" />
         <title>Bienvenue sur Cre'events !</title>
-        
+        <link id="main_style" type="text/css" rel="stylesheet" href="css/default.css" />
         <script src="js/connexion.js"></script>
+        
     </head>
     <body>
         <?php
             require("includes/header.php");
         ?>
+        
+        <div id="map-container">
+        </div>
+        <div id="search">
+            <form>
+                <input type="text" name="author" />
+                <input type="date" name="date" />
+            </form>
+        </div>
+        
+        <div id="results">
+            <span id="message"></span>
+            <ul id="event_list">
+            </ul>
+        </div>
     </body>
 </html>
