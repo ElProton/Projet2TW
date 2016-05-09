ea64a73d5101577ce1390d2b74042ebdf4b14636
Firhos <valentin.vamour@gmail.com>
2016-05-09 19:23:09 +0200

    Prise en charge du fondu du background-image sous firefox

--- js/background.js
+++ js/background.js
@@ -5,13 +5,23 @@ function init() {
     BGS = ["birthday.jpg", "candle.jpg", "city.jpg", "concert.jpg", "baloon.jpg", "cerisier.jpg", "coffee.jpg"];
     index = Math.floor((Math.random() * BGS.length));
     
-    window.setInterval(changeBG, 15000);
+    window.setInterval(hideBG, 15000);
+    window.setInterval(changeBG, 16000);
+    window.setInterval(showBG, 16000);
     changeBG();
 }
 
 function changeBG() {
     index += 1;
-    document.querySelector("html").style.backgroundImage = "url('img/"+BGS[index%BGS.length]+"')";
+    document.querySelector("#background").style.backgroundImage = "url('img/"+BGS[index%BGS.length]+"')";
+}
+
+function hideBG() {
+    document.querySelector("#background").style.opacity = "0";
+}
+
+function showBG() {
+    document.querySelector("#background").style.opacity = "1";
 }
 
 window.addEventListener("load", init);
