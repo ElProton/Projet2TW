4b2f64586b29f3fc75690bd3c618e173355bbebc
Firhos <valentin.vamour@gmail.com>
2016-05-09 23:10:23 +0200

    Modifications de la gestion d'inscription

--- includes/traitementInscription.php
+++ includes/traitementInscription.php
@@ -13,16 +13,21 @@ if (isset($_POST['username']) && isset($_POST['mdp']) && isset($_POST['mdp2'])) 
     if (trim(strlen($_POST['username'])) >= 4 && trim(strlen($_POST['username'])) <= 16 && trim(strlen($_POST['mdp'])) >= 8) {
         $username = trim($_POST['username']);
         $password = trim($_POST['mdp']);
-        $sql = "INSERT INTO users (pseudo,mdp,description) values (username=:username, password=:password, description=:description)";
-        if (isset($_POST['description']) && strlen(trim($_POST['description'])) <= 2500) {
+        $sql = "INSERT INTO users (pseudo,mdp,description) VALUES (:username, :password, :description)";
+        
+        if (isset($_POST['description'])) {
             $description = trim($_POST['description']);
         }
+        else {
+            $description = "";
+        }
+        
         $req = $bdd->prepare($sql);
         $req->execute(array("username" => $username, "password" => $password, "description" => $description));
     }
     else {
             $answer["status"] = "error";
-            $answer["results"] = array("message" => "Les données entrées ne repondent pas au critères");
+            $answer["results"] = array("message" => "Les données entrées ne repondent pas aux critères");
         }
 }
 
